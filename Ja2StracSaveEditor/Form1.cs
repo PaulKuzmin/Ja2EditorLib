8fe91d3fd93acbbe447168e9aafcc449ce1220d4
PaulKuzmin <pavel@kuzmin.online>
2025-05-16 17:17:47 +1000

    Alpha

--- Ja2StracSaveEditor/Form1.cs
+++ Ja2StracSaveEditor/Form1.cs
@@ -2,7 +2,6 @@ using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Reflection;
 using System.Text;
-using System.Windows.Forms;
 using Ja2StracSaveEditorLib;
 using Ja2StracSaveEditorLib.Managers;
 using Newtonsoft.Json;
